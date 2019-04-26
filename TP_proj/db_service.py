96baa63faa6d3e774f652c5df7d287dc879f295b
thd-nks <sherbakov_n@mail.ru>
2019-04-26 15:34:17 +0300

    Added database service interface

---
+++ TP_proj/db_service.py
@@ -0,0 +1,12 @@
+class DB:
+    def __init__(self):
+        pass
+
+    def get_user(self, id):
+        pass
+
+    def update_user(self, id):
+        pass
+
+    def get_chat(self, id1, id2):
+        pass
