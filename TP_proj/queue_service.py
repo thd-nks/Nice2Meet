7da0e4d433830fb21b4b0527ad7a96c612712763
thd-nks <sherbakov_n@mail.ru>
2019-05-27 00:33:01 +0300

    Forming queue

--- TP_proj/queue_service.py
+++ TP_proj/queue_service.py
@@ -1,9 +1,22 @@
 from reviewed_users import ReviewedUsers
+from models import *
+from db_service import DB
+
+db = DB()
 
 
 class QueueService:
     def __init__(self):
+        self.queue = []
         pass
 
-    def form_queue(self):
-        pass
+    def form_queue(self, id_user):
+        #User.create(name="Никита", sex=True, age=24, rating=0, location='', info='student', picture='', login='sasa', password='sa')
+        self.queue = db.get_couples(id_user)
+        [print(sel) for sel in self.queue]
+        return self.queue
+
+
+if __name__ == '__main__':
+    service = QueueService()
+    service.form_queue(1)
