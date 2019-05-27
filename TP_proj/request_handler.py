b6180e738377ff70f262eb999e32ad72fd74c23c
thd-nks <sherbakov_n@mail.ru>
2019-05-28 02:01:40 +0300

    Updated request service

--- TP_proj/request_handler.py
+++ TP_proj/request_handler.py
@@ -1,6 +1,13 @@
+from app import reviewed, inform, queue_service
+
+
 class RequestHandler:
     def __init__(self):
         pass
 
-    def handle_request(self):
-        pass
+    def handle_request(self, is_liked: bool, id_viewer, id_viewed):
+        reviewed.set_viewed(id_viewer, id_viewed)
+        if is_liked:
+            inform.add_to_liked(id_viewer, id_viewed)
+        return queue_service.get_from_queue()
+
