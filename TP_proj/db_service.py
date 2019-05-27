dddfa180fe84fcf6359bd3578ff2f9764f59e889
thd-nks <sherbakov_n@mail.ru>
2019-05-28 01:56:35 +0300

    Updated database service

--- TP_proj/db_service.py
+++ TP_proj/db_service.py
@@ -16,6 +16,12 @@ class DB:
                                           User.location == user.location,
                                           User.id.not_in(viewed))
 
+    def get_likes(self, id_user):
+        return LikedUser.select(LikedUser.idliker).where(LikedUser.idliked == id_user)
+
+    def add_viewed(self, id_viewer, id_viewed):
+        ViewedUser.create(idviewer=id_viewer, idviewed=id_viewed)
+
     def update_user(self, id):
         pass
 
